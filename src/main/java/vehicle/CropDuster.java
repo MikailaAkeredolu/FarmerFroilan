d7a4a43456926266a7812204c31be4f8b4e26d58
serafin544 <serafinthedream@outlook.com>
2019-09-27 16:08:09 -0400

    another push

--- src/main/java/vehicle/CropDuster.java
+++ src/main/java/vehicle/CropDuster.java
@@ -1,6 +1,16 @@
 package vehicle;
 
+import persons.Pilot;
+
 public class CropDuster extends vehicle implements AirCraft{
+
+
+    private Pilot pilot;
+
+
+    public void pilot(Pilot pilot){
+       this.pilot = pilot;
+    }
     public static void fertilize(String tmp){
 
     }
