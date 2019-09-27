3a9a0a6204926cfea891707003c2024717c93b82
hauntix <c.estrada@email.com>
2019-09-27 16:44:49 -0400

    added missing classes

--- src/main/java/persons/Farmer.java
+++ src/main/java/persons/Farmer.java
@@ -1,22 +1,34 @@
 package persons;
 
 import interfaces.Botanist;
+import interfaces.Edible;
 import interfaces.Rider;
-import interfaces.Ridable;
+import interfaces.Rideable;
 import containers.CropRow;
 
 public class Farmer extends Person implements Rider, Botanist {
-    public void mount(Ridable ride){
+    public void mount(Rideable ride){
 
     };
-    public void dismount(Ridable ride){
+    public void dismount(Rideable ride){
 
     };
-    public void ride(Ridable ride){
+    public void ride(Rideable ride){
 
     };
 
     public void plantCrop(CropRow cropRow){
 
     }
+
+    @Override
+    public void eat(Edible edible) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("YeeHaw");
+    }
+
 }
