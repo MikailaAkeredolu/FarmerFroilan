590d5b39813db12f176f7b41e8766533374d0a8f
Sarah <sarahhargreaves10@gmail.com>
2019-09-27 16:53:13 -0400

    Fixed crop generation

--- src/main/java/containers/CropRow.java
+++ src/main/java/containers/CropRow.java
@@ -1,21 +1,8 @@
 package containers;
 
-import crops.TomatoPlant;
-
 import java.util.ArrayList;
 import crops.*;
 
 public class CropRow {
-	ArrayList<Crop> crops = new ArrayList<Crop>();
-    public CropRow(String typeOfCrop, int numOfCrop) {
-        if (typeOfCrop.equals("CornStalk")){
-            for (int i=0; i<numOfCrop; i++){
-                crops.add(new CornStalk());
-            }
-        } else if (typeOfCrop.equals("TomatoPlant")){
-            for (int i=0; i<numOfCrop; i++){
-                crops.add(new TomatoPlant());
-            }
-        }
-    }
+	public ArrayList<Crop> crops = new ArrayList<Crop>();
 }
