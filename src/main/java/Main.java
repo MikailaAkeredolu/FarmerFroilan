9356a5f42b8f10aedacd563f4e2543fa8c616212
hauntix <c.estrada@email.com>
2019-09-27 15:12:38 -0400

    aaaaaaaaa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,15 @@
-public class Main {
+import containers.Farm;
+import persons.Farmer;
+import persons.Person;
+import persons.Pilot;
+import vehicle.CropDuster;
 
+public class Main {
+	public static void main(String[] args) {
+		Farmer froiland = new Farmer();
+		Farm farm  = new Farm(froiland);
+		Pilot froilanda = new Pilot();
+		CropDuster cropDuster = new CropDuster(froilanda);
+
+	}
 }
--- src/main/java/vehicle/CropDuster.java
+++ src/main/java/vehicle/CropDuster.java
@@ -1,6 +1,14 @@
 package vehicle;
 
+import persons.Pilot;
+
 public class CropDuster {
+    private Pilot pilot;
+
+    public CropDuster(Pilot pilot){
+        this.pilot = pilot;
+    }
+
     public static void fertilize(String tmp){
 
     }
